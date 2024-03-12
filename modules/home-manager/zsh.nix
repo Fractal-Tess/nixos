73aa580d91b6ef00944e6eb77a8f59abbb87ebbd
Fractal-Tess <vgfractal@gmail.com>
2024-03-12 16:15:46 +0200

    83

--- modules/home-manager/zsh.nix
+++ modules/home-manager/zsh.nix
@@ -26,17 +26,33 @@
       enable = true;
       plugins = [
         "git"
-        # "zsh-autosuggestions"
-        # "zsh-syntax-highlighting"
         "sudo"
         "zsh-navigation-tools"
       ];
     };
 
     shellAliases = {
-      update = "~/nixos/update.sh";
+      # update = "~/nixos/update.sh";
       ls = "eza";
       ll = "eza -l";
+      cat = "bat";
+      update = ''
+        set -e
+        pushd ~/nixos
+        nixpkgs-fmt . &>/dev/null
+        # git diff -U0 *.nix
+        git diff -U0 ':!*.log'
+        echo "Rebuilding NixOS..."
+        sudo nixos-rebuild switch &>nixos-switch.log || (cat nixos-switch.log | rg error && false)
+        gen=$(nixos-rebuild list-generations | head -n 2 | tail -1 | awk '{print $1}')
+        git add .
+        git commit -m "$gen"
+        # try pushing to origin but don't fail if git exists with 1
+        # print a message if it fails
+        echo "Trying to push commit to origin..."
+        git push origin main || echo "Failed to push to origin"
+        popd
+      '';
     };
 
     history.size = 10000;
