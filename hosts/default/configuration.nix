8500927db2b19ecd797396674f4b81467ef720a7
Fractal-Tess <vgfractal@gmail.com>
2024-03-12 19:33:19 +0200

    96

--- hosts/default/configuration.nix
+++ hosts/default/configuration.nix
@@ -34,8 +34,8 @@
   services.actkbd = {
     enable = true;
     bindings = [
-      { keys = [ 63 ]; events = [ "key" ]; command = "/run/current-system/sw/bin/light -A 10"; }
-      { keys = [ 64 ]; events = [ "key" ]; command = "/run/current-system/sw/bin/light -U 10"; }
+      { keys = [ 64 ]; events = [ "key" ]; command = "/run/current-system/sw/bin/light -A 10"; }
+      { keys = [ 63 ]; events = [ "key" ]; command = "/run/current-system/sw/bin/light -U 10"; }
     ];
   };
 
