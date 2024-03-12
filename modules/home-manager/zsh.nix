24fcc476deb15199cf07b11e24d6c323491ccd40
Fractal-Tess <vgfractal@gmail.com>
2024-03-12 16:20:33 +0200

    83

--- modules/home-manager/zsh.nix
+++ modules/home-manager/zsh.nix
@@ -39,23 +39,24 @@
     };
     shellGlobalAlias =
       {
-        update = ''
-          set -e
-          pushd ~/nixos
-          nixpkgs-fmt . &>/dev/null
-          # git diff -U0 *.nix
-          git diff -U0 ':!*.log'
-          echo "Rebuilding NixOS..."
-          sudo nixos-rebuild switch &>nixos-switch.log || (cat nixos-switch.log | rg error && false)
-          gen=$(nixos-rebuild list-generations | head -n 2 | tail -1 | awk '{print $1}')
-          git add .
-          git commit -m "$gen"
-          # try pushing to origin but don't fail if git exists with 1
-          # print a message if it fails
-          echo "Trying to push commit to origin..."
-          git push origin main || echo "Failed to push to origin"
-          popd
-        '';
+        # update = ''
+        #   set -e
+        #   pushd ~/nixos
+        #   nixpkgs-fmt . &>/dev/null
+        #   # git diff -U0 *.nix
+        #   git diff -U0 ':!*.log'
+        #   echo "Rebuilding NixOS..."
+        #   sudo nixos-rebuild switch &>nixos-switch.log || (cat nixos-switch.log | rg error && false)
+        #   gen=$(nixos-rebuild list-generations | head -n 2 | tail -1 | awk '{print $1}')
+        #   git add .
+        #   git commit -m "$gen"
+        #   # try pushing to origin but don't fail if git exists with 1
+        #   # print a message if it fails
+        #   echo "Trying to push commit to origin..."
+        #   git push origin main || echo "Failed to push to origin"
+        #   popd
+        # '';
+        update = "echo hello";
       };
 
     history.size = 10000;
