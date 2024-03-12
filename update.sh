b8e2f6ab690e696dcc6a0bc12c6f0b7c04648641
Fractal-Tess <vgfractal@gmail.com>
2024-03-12 15:59:40 +0200

    83

--- update.sh
+++ update.sh
@@ -7,4 +7,8 @@ sudo nixos-rebuild switch &>nixos-switch.log || (cat nixos-switch.log | rg error
 gen=$(nixos-rebuild list-generations | head -n 2 | tail -1 | awk '{print $1}')
 git add .
 git commit -m "$gen"
+# try pushing to origin but don't fail if git exists with 1
+# print a message if it fails
+echo "Trying to push commit to origin..."
+git push origin main || echo "Failed to push to origin"
 popd
