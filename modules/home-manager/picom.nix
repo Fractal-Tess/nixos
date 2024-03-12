d168a609a5d5827268c204ea398a6d72b0f0bc08
Fractal-Tess <vgfractal@gmail.com>
2024-03-13 01:15:50 +0200

    118

--- modules/home-manager/picom.nix
+++ modules/home-manager/picom.nix
@@ -3,11 +3,11 @@
     enable = true;
     activeOpacity = 1.0;
     inactiveOpacity = 0.85;
-    # settings = {
-    #   method = "guassian";
-    #   size = 10;
-    #   deviation = 5.0;
-    # };
+    settings = {
+      method = "guassian";
+      size = 10;
+      deviation = 5.0;
+    };
   };
 
 }
