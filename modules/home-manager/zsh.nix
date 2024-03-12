d6e8dab0f66d3e57c935b31fb2f303680a1a1e4b
Fractal-Tess <vgfractal@gmail.com>
2024-03-12 17:10:20 +0200

    90

--- modules/home-manager/zsh.nix
+++ modules/home-manager/zsh.nix
@@ -36,22 +36,8 @@
       ls = "eza";
       ll = "eza -l";
       cat = "bat";
-      update = ''
-        pushd ~/nixos
-        nixpkgs-fmt . &>/dev/null
-        # git diff -U0 *.nix
-        git diff -U0 ':!*.log'
-        echo "Rebuilding NixOS..."
-        sudo nixos-rebuild switch --flake ~/nixos &>nixos-switch.log || (cat nixos-switch.log | rg error && false)
-        gen=$(nixos-rebuild list-generations | head -n 2 | tail -1 | awk '{print $1}')
-        git add .
-        git commit -m "$gen"
-        # try pushing to origin but don't fail if git exists with 1
-        # print a message if it fails
-        echo "Trying to push commit to origin..."
-        git push origin main || echo "Failed to push to origin"
-        popd
-      '';
+      update = "~/nixos/update.sh";
+
     };
 
     history.size = 10000;
