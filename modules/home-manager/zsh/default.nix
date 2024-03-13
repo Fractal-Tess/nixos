a76eee10a3f984a8ad7ec867664fcaa6060fdc05
Fractal-Tess <vgfractal@gmail.com>
2024-03-13 23:30:42 +0200

    147

--- modules/home-manager/zsh/default.nix
+++ modules/home-manager/zsh/default.nix
@@ -41,6 +41,7 @@
       ndr = "nix develop ~/nixos/shells/rust";
       ndt = "nix develop ~/nixos/shells/tauri";
       ndn = "nix develop ~/nixos/shells/node";
+      ndp = "nix develop ~/nixos/shells/python";
       rt = "trash";
     };
 
