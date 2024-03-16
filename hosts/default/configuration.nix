a9c91dc592a7bcb9d3d3deb773e85578b0662388
Fractal-Tess <vgfractal@gmail.com>
2024-03-16 22:14:46 +0200

    163

--- hosts/default/configuration.nix
+++ hosts/default/configuration.nix
@@ -30,14 +30,6 @@
 
   # Flakes
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
-  programs.light.enable = true; # Needed for the /run/wrappers/bin/light SUID wrapper.
-  services.actkbd = {
-    enable = true;
-    bindings = [
-      { keys = [ 224 ]; events = [ "key" ]; command = "/run/wrappers/bin/light -A 10"; }
-      { keys = [ 225 ]; events = [ "key" ]; command = "/run/wrappers/bin/light -U 10"; }
-    ];
-  };
 
   # Set your time zonesudo
   time.timeZone = "Europe/Sofia";
