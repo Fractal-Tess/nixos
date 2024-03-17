53e885788d7d3a06e42ea250cef68a2907211da5
Fractal-Tess <vgfractal@gmail.com>
2024-03-17 04:20:11 +0200

    176

--- hosts/default/configuration.nix
+++ hosts/default/configuration.nix
@@ -100,7 +100,7 @@
     alsa.support32Bit = true;
     pulse.enable = true;
     # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
+    jack.enable = true;
 
     # use the example session manager (no others are packaged yet so this is enabled by default,
     # no need to redefine it in your config for now)
@@ -134,6 +134,7 @@
   # List packages installed in system profile. To search, run:
   environment.systemPackages = with pkgs; [
 
+    pulseaudio
     # GUI
     polybarFull
     nomacs
