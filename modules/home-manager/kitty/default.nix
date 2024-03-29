622e1bf459675432d50824f629e5370432b3ea9f
Fractal-Tess <vgfractal@gmail.com>
2024-03-29 22:18:21 +0200

    194

--- modules/home-manager/kitty/default.nix
+++ modules/home-manager/kitty/default.nix
@@ -5,12 +5,13 @@
       package = null;
       # Download font from here https://github.com/ryanoasis/nerd-fonts/releases/download/v3.1.1/CascadiaCode.zip
       name = "CaskaydiaCoveNerdFontMono";
-      size = 11;
+      size = 12;
     };
     settings = {
       scrollback_lines = 10000;
-      background_opacity = "0.85";
-      background_blur = 64;
+      # background_opacity = "0.85";
+      background_opacity = "1.0";
+      # background_blur = 64;
       update_check_interval = 0;
       enable_audio_bell = false;
       disable_ligatures = "never";
