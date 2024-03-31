461d7c6ed9f11aebe253d8e5e40ddbdc6b8057b9
Fractal-Tess <vgfractal@gmail.com>
2024-04-01 00:59:31 +0300

    198

--- modules/home-manager/warp-terminal/default.nix
+++ modules/home-manager/warp-terminal/default.nix
@@ -1,6 +1,5 @@
 { pkgs, ... }: {
   home.packages = with pkgs; [ warp-terminal ];
-  home.file.".config/warp-terminal/user_preferences.json".source = {
-    source = ./warp-terminal/user_preferences.json;
-  };
+  home.file.".config/warp-terminal/user_preferences.json".source = ./warp-terminal/user_preferences.json;
+
 }
