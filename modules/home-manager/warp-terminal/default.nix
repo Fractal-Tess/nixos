4fab85696c4a31f86860bdf93df6b306946e3d72
Fractal-Tess <vgfractal@gmail.com>
2024-04-01 02:05:17 +0300

    202

--- modules/home-manager/warp-terminal/default.nix
+++ modules/home-manager/warp-terminal/default.nix
@@ -1,6 +1,6 @@
 { pkgs, ... }: {
   home.packages = with pkgs; [ warp-terminal ];
-  home.file.".config/warp-terminal" = {
+  home.file.".config/test" = {
     source = ./warp-terminal;
     recursive = true;
   };
