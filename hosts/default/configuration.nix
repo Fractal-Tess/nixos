69f8a36547f6ad61cb81617cbc0453625a6bc105
Fractal-Tess <vgfractal@gmail.com>
2024-04-10 19:36:42 +0300

    214

--- hosts/default/configuration.nix
+++ hosts/default/configuration.nix
@@ -42,8 +42,8 @@
   services.actkbd = {
     enable = true;
     bindings = [
-      { keys = [ 224 ]; events = [ "key" ]; command = "/run/current-system/sw/bin/light -U 10"; }
-      { keys = [ 225 ]; events = [ "key" ]; command = "/run/current-system/sw/bin/light -A 10"; }
+      { keys = [ 224 ]; events = [ "key" ]; command = "/etc/profiles/per-user/fractal-tess/bin/light -U 10"; }
+      { keys = [ 225 ]; events = [ "key" ]; command = "/etc/profiles/per-user/fractal-tess/bin/light -A 10"; }
     ];
   };
 
