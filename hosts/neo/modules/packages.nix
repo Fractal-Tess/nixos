2a60f89346cd15411abd73dd50aca7eedf76bc08
Fractal-Tess <vgfractal@gmail.com>
2024-09-30 16:13:30 +0300

    redo neo

--- hosts/neo/modules/packages.nix
+++
@@ -1,9 +0,0 @@
-{ pkgs, ... }: {
-  nixpkgs.config.permittedInsecurePackages = [
-    "electron-27.3.11"
-  ];
-
-  nixpkgs.config.allowUnfree = true;
-
-  environment.systemPackages = with pkgs; [ ];
-}
