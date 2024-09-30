a6705bba47bbad8b74de351b56b43d6baf23bf17
Fractal-Tess <vgfractal@gmail.com>
2024-09-30 16:13:30 +0300

    redo neo

--- hosts/neo/configuration.nix
+++ hosts/neo/configuration.nix
@@ -1,67 +1,193 @@
-{ pkgs, inputs, ... }:
+{ pkgs, inputs, username, ... }:
 {
   imports =
     [
-      # Include the results of the hardware scan.
       ./hardware-configuration.nix
       inputs.home-manager.nixosModules.default
-      ../../modules/nixos/sddm/default.nix
-      ../../modules/nixos/auto-cpufreq/default.nix
-      ./modules/audio.nix
-      ./modules/boot.nix
-      ./modules/display-protocol.nix
-      ./modules/misc.nix
-      ./modules/networking.nix
-      ./modules/packages.nix
-      ./modules/time-and-locale.nix
+
+      ../../modules/nixos/audio/default.nix
+      ../../modules/nixos/boot/default.nix
+      ../../modules/nixos/networking/default.nix
+      ../../modules/nixos/nvidia/default.nix
+      ../../modules/nixos/display/all.nix
+      ../../modules/nixos/programs/all.nix
+      ../../modules/nixos/services/all.nix
     ];
 
+  # Flakes
   nix.settings = {
-    # Flakes
     experimental-features = [ "nix-command" "flakes" ];
     auto-optimise-store = true;
   };
 
 
-  # Define a user account. Don't forget to set a password with ‘passwd’.
+  # Packages and overlays
+  nixpkgs.overlays = [ inputs.polymc.overlay ]; ## Within configuration.nix
+
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
+
+  # Insecure packages
+  nixpkgs.config.permittedInsecurePackages = [
+    "electron-27.3.11"
+  ];
+
+  environment.systemPackages = with pkgs; [ ];
+
+
+  # Enable audio -----------------------------------------------------------
+  modules.audio.enable = true;
+
+  # Enable networking ------------------------------------------------------
+  modules.networking = {
+    enable = true;
+    firewall = {
+      allowedPorts = [ 9 22 ];
+    };
+    vpn.netbird.enable = true;
+
+  };
+
+
+  # Enable nvidia  ---------------------------------------------------------
+  modules.hardware.nvidia.enable = true;
+
+  # Window Manager & compositor --------------------------------------------
+  modules.display.hyprland = {
+    enable = true;
+    videoDrivers = [ "nvidia" ];
+    greetd.enable = true;
+    greetd.autoLogin = true;
+  };
+  modules.display.waybar.enable = true;
+
+  # Boot -------------------------------------------------------------------
+  modules.boot.useCustomConfig = true;
+
+
+
+  # Services ---------------------------------------------------------------
+  ## Enable CUPS to print documents.
+  services.printing.enable = true;
+
+  ## Dbus
+  services.dbus.enable = true;
+
+  ## Enable the OpenSSH daemon.
+  modules.services.sshd.enable = true;
+
+  ## Add additional filesystme services
+  modules.services.filesystemExtraServices.enable = true;
+
+  ## Andorid Debug Bridge
+  modules.services.adb.enable = true;
+
+  ## Docker 
+  modules.services.docker = {
+    enable = true;
+    rootless = true;
+    nvidia = true;
+  };
+
+
+
+  # Programs ---------------------------------------------------------------
+  ## Enable direnv
+  modules.programs.direnv.enable = true;
+
+  ## Enable Yazi
+  modules.programs.yazi.enable = true;
+
+  ## Zsh
+  modules.programs.zsh.enable = true;
+
+
+
+
+  # Users  -----------------------------------------------------------------
+  ## User accounts
   users.users.fractal-tess = {
     isNormalUser = true;
-    extraGroups = [ "networkmanager" "wheel" "video" "docker" "adbusers" ];
+    extraGroups = [ "networkmanager" "wheel" "video" "docker" ];
     password = "password";
-    # packages = with pkgs; [];
-    # description = "";
+    description = "default user";
+    # packages = with pkgs; []
   };
-  # Make users mutable - allows them to change their password with passwd
+
+  ## Make users mutable 
   users.mutableUsers = true;
 
   # Home-Manger
   home-manager = {
-    backupFileExtension = "hm-backup";
     useGlobalPkgs = true;
     useUserPackages = true;
     extraSpecialArgs = {
-      inherit inputs;
+      inherit inputs username;
     };
-    users = {
-      "fractal-tess" = import ./home.nix;
-    };
+    users."${username}" = import ./home.nix;
+    backupFileExtension = "hm-bak";
   };
 
+  # Fonts
+  fonts.packages = with pkgs; [
+    font-awesome
+    powerline-fonts
+    powerline-symbols
+    (nerdfonts.override { fonts = [ "NerdFontsSymbolsOnly" ]; })
+  ];
 
 
+  # Security
   security.sudo.extraRules = [
     {
-      users = [ "fractal-tess" ];
+      users = [ username ];
       commands = [
         {
+          # Removes the need for a password when using sudo
           command = "ALL";
-          options = [ "NOPASSWD" ];
+          options = [ "NOPASSWD" ]; # "SETENV" # Adding the following could be a good idea
         }
       ];
     }
   ];
 
+  security.polkit.enable = true;
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  # };
 
 
-  system.stateVersion = "23.11";
+  # Timezone 
+  time.timeZone = "Europe/Sofia";
+
+  ## Internationalisation
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  ## Extra locale settings
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "en_US.UTF-8";
+    LC_IDENTIFICATION = "en_US.UTF-8";
+    LC_MEASUREMENT = "en_US.UTF-8";
+    LC_MONETARY = "en_US.UTF-8";
+    LC_NAME = "en_US.UTF-8";
+    LC_NUMERIC = "en_US.UTF-8";
+    LC_PAPER = "en_US.UTF-8";
+    LC_TELEPHONE = "en_US.UTF-8";
+    LC_TIME = "en_US.UTF-8";
+  };
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "24.05"; # Did you read the comment?
 }
+
+
