ecf1939afb9cefd42600500f5fa1a7219bc82cc9
Fractal-Tess <vgfractal@gmail.com>
2025-02-02 14:29:15 +0200

    s

--- hosts/vd/configuration.nix
+++ hosts/vd/configuration.nix
@@ -8,11 +8,17 @@
     ../../modules/nixos/core/networking.nix
 
     ../../modules/nixos/drivers/nvidia.nix
-
-    ../../modules/programs.nix
-    ../../modules/services.nix
     ../../modules/nixos/display/all.nix
+    ../../modules/nixos/services.nix
+    ../../modules/nixos/programs.nix
   ];
+
+  # Shell
+  programs.zsh.enable = true;
+  users.defaultUserShell = pkgs.zsh;
+  # environment.pathsToLink = [ "/share/zsh" ];
+
+  environment.systemPackages = with pkgs; [ ];
 
   # Flakes 
   nix.settings = {
@@ -158,10 +164,10 @@
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
+  programs.gnupg.agent = {
+    enable = true;
+    enableSSHSupport = true;
+  };
 
   # Timezone
   time.timeZone = "Europe/Sofia";
@@ -180,7 +186,7 @@
 
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # on your system were taken. It's perfectly fine and recommended to leave
   # this value at the release version of the first install of this system.
   # Before changing this value read the documentation for this option
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
--- modules/home-manager/warp-terminal/default.nix
+++
@@ -1,8 +0,0 @@
-{ pkgs, ... }: {
-  home.packages = with pkgs; [ warp-terminal ];
-  home.file.".config/warp-terminal" = {
-    source = ./warp-terminal;
-    recursive = true;
-  };
-
-}
