8898076399f9c0467832cbcf259992381f199ba3
Fractal-Tess <vgfractal@gmail.com>
2025-02-13 17:50:52 +0200

    121

--- shells/tauri/flake.nix
+++ shells/tauri/flake.nix
@@ -6,37 +6,39 @@
     systems.url = "github:nix-systems/default";
   };
 
-  outputs = { nixpkgs, systems, ... } @ inputs:
+  outputs = { nixpkgs, systems, ... }@inputs:
     let
       eachSystem = f:
-        nixpkgs.lib.genAttrs (import systems) (
-          system:
-          f nixpkgs.legacyPackages.${system}
-        );
+        nixpkgs.lib.genAttrs (import systems)
+          (system: f nixpkgs.legacyPackages.${system});
 
-      libraries = pkgs: with pkgs; [
-        webkitgtk
-        gtk3
-        cairo
-        gdk-pixbuf
-        glib
-        dbus
-        openssl_3
-        librsvg
-      ];
+      libraries = pkgs:
+        with pkgs; [
+          webkitgtk
+          gtk3
+          cairo
+          gdk-pixbuf
+          glib
+          dbus
+          openssl_3
+          librsvg
+        ];
 
-      packages = pkgs: with pkgs; [
-        curl
-        wget
-        pkg-config
-        dbus
-        openssl_3
-        glib
-        gtk3
-        libsoup
-        webkitgtk
-        librsvg
-      ];
+      packages = pkgs:
+        with pkgs; [
+          curl
+          wget
+          pkg-config
+          dbus
+          openssl_3
+          glib
+          gtk3
+          libsoup
+          webkitgtk
+          librsvg
+
+          lolcat
+        ];
     in
     {
       devShells = eachSystem (pkgs: {
@@ -44,10 +46,20 @@
           buildInputs = packages pkgs;
 
           shellHook = ''
-            export LD_LIBRARY_PATH=${pkgs.lib.makeLibraryPath (libraries pkgs)}:$LD_LIBRARY_PATH
+            echo "#
+            #  ______                  _ 
+            # /_  __/___ ___  _______(_)
+            #  / / / __ `/ / / / ___/ / 
+            # / / / /_/ / /_/ / /  / /  
+            #/_/  \__,_/\__,_/_/  /_/   
+            #
+            Tauri Development Environment
+            " | lolcat
+
+            export LD_LIBRARY_PATH=${
+              pkgs.lib.makeLibraryPath (libraries pkgs)
+            }:$LD_LIBRARY_PATH
             export XDG_DATA_DIRS=${pkgs.gsettings-desktop-schemas}/share/gsettings-schemas/${pkgs.gsettings-desktop-schemas.name}:${pkgs.gtk3}/share/gsettings-schemas/${pkgs.gtk3.name}:$XDG_DATA_DIRS
-            zsh
-            exit 0
           '';
         };
       });
