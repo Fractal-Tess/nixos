e82925dd2ea78e13fe0e524fb51345bc3360c6c4
Fractal-Tess <vgfractal@gmail.com>
2025-02-13 17:10:54 +0200

    118

--- shells/tauri/flake.nix
+++ shells/tauri/flake.nix
@@ -1,49 +1,55 @@
 {
+  description = "Tauri development shell";
+
   inputs = {
-    nixpkgs.url = "nixpkgs";
-    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    systems.url = "github:nix-systems/default";
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = nixpkgs.legacyPackages.${system};
+  outputs = { nixpkgs, systems, ... } @ inputs:
+    let
+      eachSystem = f:
+        nixpkgs.lib.genAttrs (import systems) (
+          system:
+          f nixpkgs.legacyPackages.${system}
+        );
 
-        libraries = with pkgs;[
-          webkitgtk
-          gtk3
-          cairo
-          gdk-pixbuf
-          glib
-          dbus
-          openssl_3
-          librsvg
-        ];
+      libraries = pkgs: with pkgs; [
+        webkitgtk
+        gtk3
+        cairo
+        gdk-pixbuf
+        glib
+        dbus
+        openssl_3
+        librsvg
+      ];
 
-        packages = with pkgs; [
-          curl
-          wget
-          pkg-config
-          dbus
-          openssl_3
-          glib
-          gtk3
-          libsoup
-          webkitgtk
-          librsvg
-        ];
-      in
-      {
-        devShell = pkgs.mkShell {
-          buildInputs = packages;
+      packages = pkgs: with pkgs; [
+        curl
+        wget
+        pkg-config
+        dbus
+        openssl_3
+        glib
+        gtk3
+        libsoup
+        webkitgtk
+        librsvg
+      ];
+    in
+    {
+      devShells = eachSystem (pkgs: {
+        default = pkgs.mkShell {
+          buildInputs = packages pkgs;
 
-          shellHook =
-            ''
-              export LD_LIBRARY_PATH=${pkgs.lib.makeLibraryPath libraries}:$LD_LIBRARY_PATH
-              export XDG_DATA_DIRS=${pkgs.gsettings-desktop-schemas}/share/gsettings-schemas/${pkgs.gsettings-desktop-schemas.name}:${pkgs.gtk3}/share/gsettings-schemas/${pkgs.gtk3.name}:$XDG_DATA_DIRS
-              zsh
-              exit 0
-            '';
+          shellHook = ''
+            export LD_LIBRARY_PATH=${pkgs.lib.makeLibraryPath (libraries pkgs)}:$LD_LIBRARY_PATH
+            export XDG_DATA_DIRS=${pkgs.gsettings-desktop-schemas}/share/gsettings-schemas/${pkgs.gsettings-desktop-schemas.name}:${pkgs.gtk3}/share/gsettings-schemas/${pkgs.gtk3.name}:$XDG_DATA_DIRS
+            zsh
+            exit 0
+          '';
         };
       });
+    };
 }
