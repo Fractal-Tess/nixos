7f15879164edbbb771fffa9a98304e64c5663623
Fractal-Tess <vgfractal@gmail.com>
2025-02-13 21:39:40 +0200

    133

--- modules/home-manager/default.nix
+++ modules/home-manager/default.nix
@@ -2,8 +2,12 @@
   # Direnv
   programs.direnv = {
     enable = true;
+    # Faster built in implemenation of direnv
     nix-direnv.enable = true;
+    # Enable direnv integration with zsh
     enableZshIntegration = true;
+    # Silent direnv env loading ouput
+    silent = true;
   };
 
   programs.zoxide = {
