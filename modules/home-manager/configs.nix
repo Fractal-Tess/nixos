2d9cf5c4fcdc7a9c5dc76e1de284589c2ae334b7
Fractal-Tess <vgfractal@gmail.com>
2025-02-28 16:44:38 +0200

    163

--- modules/home-manager/configs.nix
+++ modules/home-manager/configs.nix
@@ -1,16 +1,13 @@
 { osConfig, lib, ... }:
-with lib; {
+
+with lib;
+
+{
   home.file = {
     # # Building this configuration will create a copy of 'dotfiles/screenrc' in
     # # the Nix store. Activating the configuration will then make '~/.screenrc' a
     # # symlink to the Nix store copy.
     # ".screenrc".source = dotfiles/screenrc;
-
-    # # You can also set the file content immediately.
-    # ".gradle/gradle.properties".text = ''
-    #   org.gradle.console=verbose
-    #   org.gradle.daemon.idletimeout=3600000
-    # '';
 
     # Zsh config
     ".zshrc".source = ../../config/zsh/.zshrc;
