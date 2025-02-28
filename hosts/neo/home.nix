7937a92994967e392d4536fecd87a053436544f2
Fractal-Tess <vgfractal@gmail.com>
2025-02-28 18:17:50 +0200

    164

--- hosts/neo/home.nix
+++ hosts/neo/home.nix
@@ -1,67 +1,23 @@
-{ pkgs, username, osConfig, lib, ... }:
-with lib; {
-  imports = [ ../../modules/home-manager/default.nix ];
+{ pkgs, username, lib, osConfig, ... }:
 
-  # Home Manager 
+with lib;
+let cfg = osConfig;
+in {
+  imports = [
+    ../../modules/home-manager/default.nix
+    ../../modules/home-manager/configs.nix
+    ../../modules/home-manager/theming.nix
+  ];
+
+  # Home Manager
   home.username = username;
   home.homeDirectory = "/home/${username}";
 
-  # Theming
-  gtk = mkIf osConfig.modules.gui {
-    enable = true;
-    theme = {
-      name = "Nordic-darker";
-      package = pkgs.nordic;
-    };
-    iconTheme = {
-      name = "Nordzy-dark";
-      package = pkgs.nordzy-icon-theme;
-    };
-    cursorTheme = {
-      name = "Nordzy-cursors";
-      package = pkgs.nordzy-cursor-theme;
-      size = 32;
-    };
-  };
-
-  # qt = {
-  #   enable = true;
-  #   platformTheme.name = "gtk";
-  # };
-
-  home.stateVersion = "24.05";
-
+  # Has to exist so other home manager modules can mkMerge
   home.packages = [ ];
 
-  # Home Manager is pretty good at managing dotfiles. The primary way to manage
-  # plain files is through 'home.file'.
-  home.file = {
-    # # Building this configuration will create a copy of 'dotfiles/screenrc' in
-    # # the Nix store. Activating the configuration will then make '~/.screenrc' a
-    # # symlink to the Nix store copy.
-    # ".screenrc".source = dotfiles/screenrc;
-
-    # # You can also set the file content immediately.
-    # ".gradle/gradle.properties".text = ''
-    #   org.gradle.console=verbose
-    #   org.gradle.daemon.idletimeout=3600000
-    # '';
-
-    # Zsh - p10k config
-    ".p10k.config.zsh".source = ../../config/zsh/.p10k.config.zsh;
-  };
-
-  # Hyprland
-  xdg.configFile.hypr = mkIf osConfig.modules.display.hyprland.enable {
-    source = ../../config/hypr;
-    recursive = true;
-  };
-
-  # Waybar
-  xdg.configFile.waybar = mkIf osConfig.modules.display.waybar.enable {
-    source = ../../config/waybar;
-    recursive = true;
-  };
+  # Home manager version - don't change this
+  home.stateVersion = "24.05";
 
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
