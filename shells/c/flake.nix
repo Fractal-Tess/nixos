61c9a9bcdf05a9e5ab27bda2599ea65665cfa748
Fractal-Tess <vgfractal@gmail.com>
2025-03-24 04:05:04 +0200

    updating shells

--- shells/c/flake.nix
+++ shells/c/flake.nix
@@ -3,63 +3,68 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    systems.url = "github:nix-systems/default";
   };
 
-  outputs = { self, nixpkgs }:
+  outputs = { self, nixpkgs, systems }:
+    let eachSystem = nixpkgs.lib.genAttrs (import systems);
+    in {
+      devShells = eachSystem (system:
+        let
+          pkgs = import nixpkgs {
+            inherit system;
+            config = { allowUnfree = true; };
+          };
+          llvm = pkgs.llvmPackages_19; # Change this to update the whole stack
+          clang = pkgs.clang_19;
+        in {
+          default = pkgs.mkShell {
+            shellHook = ''
+              echo "
+                 ______
+                / ____/
+               / /     
+              / /___   
+              \____/   
+              clang version: $(${clang}/bin/clang --version | head -n 1)
+              " | ${pkgs.lolcat}/bin/lolcat
+              zsh;
+              exit 0;
+            '';
+            packages = with pkgs;
+              [
+                clang
 
-    let
+                # Build tools
+                # gnumake
+                # cmake
+                # bear
 
-      supportedSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
-      forEachSupportedSystem = f: nixpkgs.lib.genAttrs supportedSystems (system: f rec {
-        pkgs = nixpkgs.legacyPackages.${system};
-        llvm = pkgs.llvmPackages_18;
-      });
+                # Debuggers
+                # llvm.lldb
+                # gdb
 
-    in
-    {
-      devShells = forEachSupportedSystem
-        ({ pkgs, llvm }: {
-          default = pkgs.mkShell.override
-            {
-              stdenv = pkgs.clangStdenv;
-            }
-            rec
-            {
-              # TODO: Remove this shellHook - This here is bad because it assumes that zsh is installed and is prefered 
-              shellHook = ''
-                zsh
-                exit
-              '';
-              packages = with pkgs; [
-                # builder
-                gnumake
-                cmake
-                bear
-
-                # debugger
-                llvm.lldb
-                gdb
-
-                # fix headers not found
-                clang-tools
+                # Fix headers not found
+                # clang-tools
 
                 # lsp and compiler
-                llvm.libstdcxxClang
+                # llvm.libstdcxxClang
 
                 # other tools
-                cppcheck
-                llvm.libllvm
-                valgrind
+                # cppcheck
+                # llvm.libllvm
+                # valgrind
 
                 # stdlib for cpp
-                llvm.libcxx
+                # llvm.libcxx
 
                 # libs
-                glm
-                SDL2
-                SDL2_gfx
+                # glm
+                # SDL2
+                # SDL2_gfx
               ];
-            };
+
+          };
         });
     };
 }
