13c0cdea4930ce87f8e95773a6d4a8ee58a502ec
Fractal-Tess <vgfractal@gmail.com>
2025-05-06 16:58:52 +0300

    366

--- hosts/vd/hardware-configuration.nix
+++ hosts/vd/hardware-configuration.nix
@@ -9,8 +9,8 @@
   boot.initrd.availableKernelModules =
     [ "nvme" "xhci_pci" "ahci" "usb_storage" "usbhid" "sd_mod" ];
   boot.initrd.kernelModules = [ ];
-  p
-    boot.kernelModules = [ "kvm-amd" ];
+
+  boot.kernelModules = [ "kvm-amd" ];
   boot.extraModulePackages = [ ];
 
   fileSystems."/" = {
