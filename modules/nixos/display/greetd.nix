89c22ab60f6b545b38b48e2b8a101a806bca6972
Fractal-Tess <vgfractal@gmail.com>
2025-05-08 00:44:44 +0300

    374

--- modules/nixos/display/greetd.nix
+++ modules/nixos/display/greetd.nix
@@ -1,1 +1,42 @@
+{ config, lib, pkgs, username, ... }:
 
+with lib;
+
+let cfg = config.modules.display.greetd;
+in {
+  options.modules.display.greetd = {
+    enable = mkEnableOption "Greetd display manager";
+
+    autoLogin = mkEnableOption "Automatic login without password prompt";
+
+    session = {
+      command = mkOption {
+        type = types.str;
+        description = "Command to start the session";
+        default = "";
+      };
+
+      user = mkOption {
+        type = types.str;
+        description = "User to automatically log in";
+        default = username;
+      };
+    };
+  };
+
+  config = mkIf cfg.enable {
+    services.greetd = {
+      enable = true;
+      settings = {
+        default_session = {
+          command = mkIf (cfg.session.command != "") cfg.session.command;
+        };
+
+        initial_session = mkIf cfg.autoLogin {
+          command = cfg.session.command;
+          user = cfg.session.user;
+        };
+      };
+    };
+  };
+}
