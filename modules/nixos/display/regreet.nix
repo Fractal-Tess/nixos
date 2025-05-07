0220c84b5282672264406257abf56fa6c85ca590
Fractal-Tess <vgfractal@gmail.com>
2025-05-08 01:57:36 +0300

    379

--- modules/nixos/display/regreet.nix
+++ modules/nixos/display/regreet.nix
@@ -25,19 +25,19 @@ in
         "ReGreet is configured to use Hyprland, but Hyprland is not enabled. Please enable modules.display.hyprland.";
     }];
 
-    # services.greetd = {
-    #   enable = true;
-    #   settings = mkIf cfg.autoLogin {
-    #     initial_session = {
-    #       command = "${config.programs.hyprland.package}/bin/Hyprland";
-    #       user = username;
-    #     };
-    #     default_session = {
-    #       command = "${config.programs.regreet.package}/bin/regreet";
-    #       user = "greeter";
-    #     };
-    #   };
-    # };
+    services.greetd = {
+      enable = true;
+      settings = mkIf cfg.autoLogin {
+        initial_session = {
+          command = "${config.programs.hyprland.package}/bin/Hyprland";
+          user = username;
+        };
+        default_session = {
+          command = "${config.programs.regreet.package}/bin/regreet";
+          user = "greeter";
+        };
+      };
+    };
 
     # Enable regreet as the greeter
     programs.regreet = {
@@ -50,42 +50,23 @@ in
       theme = { name = username; };
 
       # Set the session to Hyprland
-      settings = ''
-        [background]
-        path = "/home/fractal-tess/nixos/backgrounds/1.jpg"
-        fit = "Cover"
-
-        [commands]
-        reboot = ["systemctl", "reboot"]
-        poweroff = ["systemctl", "poweroff"]
-
-        [appearance]
-        greeting_msg = "Welcome back, ${username}!"
-
-        [widget.clock]
-        format = "%a %H:%M"
-        resolution = "500ms"
-        timezone = "${config.time.timeZone}"
-        label_width = 150
-
-        [GTK]
-        # Whether to use the dark theme
-        application_prefer_dark_theme = true
-
-        # Cursor theme name
-        cursor_theme_name = "Adwaita"
-
-        # Font name and size
-        font_name = "Cantarell 16"
-
-        # Icon theme name
-        icon_theme_name = "Adwaita"
-
-        # GTK theme name
-        theme_name = "Adwaita"
-
-      '';
-
+      settings = {
+        background = {
+          path = "/usr/share/backgrounds/greeter.jpg";
+          fit = "Cover";
+        };
+        commands = {
+          reboot = [ "systemctl" "reboot" ];
+          poweroff = [ "systemctl" "poweroff" ];
+        };
+        appearance = { greeting_msg = "Welcome back, ${username}!"; };
+        "widget.clock" = {
+          format = "%a %H:%M";
+          resolution = "500ms";
+          timezone = config.time.timeZone;
+          label_width = 150;
+        };
+      };
     };
   };
 }
