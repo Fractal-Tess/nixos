7cc9b2070d99c6227e92303202f139dd3316e9ea
Fractal-Tess <vgfractal@gmail.com>
2025-05-07 23:56:33 +0300

    370

---
+++ modules/nixos/services/default.nix
@@ -0,0 +1,13 @@
+{ config, lib, pkgs, ... }:
+
+with lib;
+
+{
+  imports = [
+    ./adb/default.nix
+    ./auto_cpu/default.nix
+    ./filesystemExtraServices/default.nix
+    ./sshd/default.nix
+    ./docker/default.nix
+  ];
+}
