951aa47dfdda718660ca75eadc1927f76b7f3fd3
Fractal-Tess <vgfractal@gmail.com>
2025-05-21 08:28:45 +0300

    195

--- overlays/ulauncher-webkitgtk.nix
+++ overlays/ulauncher-webkitgtk.nix
@@ -1,6 +1,6 @@
 self: super: {
   ulauncher_patched = super.ulauncher.overrideAttrs (old: {
-    buildInputs = (old.buildInputs or [ ]) ++ [ super.webkitgtk_4_0 ];
+    buildInputs = (old.buildInputs or [ ]) ++ [ super.webkitgtk_4_1 ];
     postInstall = (old.postInstall or "") + ''
       wrapProgram $out/bin/ulauncher \
         --set GI_TYPELIB_PATH "${super.webkitgtk_4_1}/lib/girepository-1.0"
