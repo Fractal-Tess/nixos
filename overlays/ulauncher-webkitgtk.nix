d45b727f5b612169f73e4b4940da2a448247f48a
Fractal-Tess <vgfractal@gmail.com>
2025-05-21 08:43:54 +0300

    196

--- overlays/ulauncher-webkitgtk.nix
+++ overlays/ulauncher-webkitgtk.nix
@@ -1,9 +1,9 @@
 self: super: {
   ulauncher_patched = super.ulauncher.overrideAttrs (old: {
-    buildInputs = (old.buildInputs or [ ]) ++ [ super.webkitgtk_4_1 ];
+    buildInputs = (old.buildInputs or [ ]) ++ [ super.webkitgtk_4_0 ];
     postInstall = (old.postInstall or "") + ''
       wrapProgram $out/bin/ulauncher \
-        --set GI_TYPELIB_PATH "${super.webkitgtk_4_1}/lib/girepository-1.0"
+        --set GI_TYPELIB_PATH "${super.webkitgtk_4_0}/lib/girepository-1.0"
     '';
   });
 }
