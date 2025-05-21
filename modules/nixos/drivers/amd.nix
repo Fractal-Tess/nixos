e5935d9ccd04ac412b455f6edb9bdf926cc16dba
Fractal-Tess <vgfractal@gmail.com>
2025-05-21 08:13:34 +0300

    194

--- modules/nixos/drivers/amd.nix
+++ modules/nixos/drivers/amd.nix
@@ -3,27 +3,15 @@
 with lib;
 
 let cfg = config.modules.drivers.amd;
+
 in {
   options.modules.drivers.amd = { enable = mkEnableOption "AMD GPU drivers"; };
 
   config = mkIf cfg.enable {
     # Add AMD driver for Xorg and Wayland
     services.xserver.videoDrivers = mkDefault [ "amdgpu" ];
-
-    hardware.opengl = {
-      # Enable OpenGL support
-      enable = mkDefault true;
-      # Enable 32-bit support for OpenGL (needed for some applications)
-      driSupport32Bit = mkDefault true;
-      # Add extra packages for AMD GPU support
-      extraPackages = with pkgs; [ amdvlk rocm-opencl-icd rocm-opencl-runtime ];
-      # Add 32-bit versions of the packages
-      extraPackages32 = with pkgs.driversi686Linux; [ amdvlk ];
-    };
-
-    # Enable early KMS for better resolution during boot
+    # Enable early KMS for better ret
     boot.initrd.kernelModules = [ "amdgpu" ];
-
     # Enable firmware for AMD GPUs
     hardware.enableRedistributableFirmware = mkDefault true;
   };
