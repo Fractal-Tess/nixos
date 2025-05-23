14d71d5cc41b748ef2aa4806073ce47a40c616a9
Fractal-Tess <vgfractal@gmail.com>
2025-05-23 22:19:49 +0300

    398

--- modules/nixos/display/regreet.nix
+++ modules/nixos/display/regreet.nix
@@ -46,6 +46,48 @@ in
 
       cageArgs = [ "-m" "last" ];
 
+      extraCss = ''
+        /* Base dark theme variables */
+        :root {
+          --background-color: #121212;
+          --text-color: #eee;
+          --primary-color: #809fff;
+          --font-family: 'Noto Sans', sans-serif;
+          --font-size: 16px;
+        }
+
+        /* Apply dark theme to Regreet container */
+        .regreet-dark-theme {
+          background-color: var(--background-color);
+          color: var(--text-color);
+          font-family: var(--font-family);
+          font-size: var(--font-size);
+        }
+
+        /* Style Regreet internal parts */
+        regreet-component::part(header),
+        regreet-component::part(footer),
+        regreet-component::part(content) {
+          background-color: var(--background-color);
+          color: var(--text-color);
+        }
+
+        /* Links and buttons */
+        .regreet-dark-theme a {
+          color: var(--primary-color);
+          text-decoration: none;
+        }
+
+        .regreet-dark-theme button {
+          background-color: var(--primary-color);
+          color: var(--background-color);
+          border: none;
+          padding: 0.5em 1em;
+          border-radius: 4px;
+          cursor: pointer;
+        }
+      '';
+
       # Set the session to Hyprland
       settings = {
         background = {
@@ -57,12 +99,6 @@ in
           poweroff = [ "systemctl" "poweroff" ];
         };
         appearance = { greeting_msg = "Welcome back, ${username}!"; };
-        GTK = {
-          application_prefer_dark_theme = mkForce true;
-          cursor_theme_name = mkForce "Nordzy-cursors";
-          icon_theme_name = mkForce "Nordic";
-          theme_name = mkForce "Nordic";
-        };
         "widget.clock" = {
           format = "%a %H:%M";
           resolution = "500ms";
