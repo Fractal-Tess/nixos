fb3af86f2234814649156dae2f270dddb96061d3
Fractal-Tess <vgfractal@gmail.com>
2025-07-01 05:41:04 +0300

    Replace filesystemExtraServices with automount service in configuration and add new automount module.

--- modules/nixos/services/default.nix
+++ modules/nixos/services/default.nix
@@ -4,7 +4,7 @@
   imports = [
     ./adb/default.nix
     ./auto_cpu/default.nix
-    ./filesystemExtraServices/default.nix
+    ./automount/default.nix
     ./sshd/default.nix
     ./docker/default.nix
     ./smb/default.nix
