955e74b8e0b7b6d4305e9bab0af44f0295e071c8
Fractal-Tess <vgfractal@gmail.com>
2025-07-04 06:41:56 +0300

    411

---
+++ overlays/responsively-app.nix
@@ -0,0 +1,33 @@
+self: super: {
+  # Overlay for Responsively App v1.16.0 using the official AppImage
+  responsively-app = super.stdenv.mkDerivation rec {
+    pname = "responsively-app";
+    version = "1.16.0";
+
+    src = super.fetchurl {
+      url =
+        "https://github.com/responsively-org/responsively-app-releases/releases/download/v1.16.0/ResponsivelyApp-1.16.0.AppImage";
+      sha256 =
+        "sha256-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA="; # TODO: Replace with real hash
+    };
+
+    dontUnpack = true;
+
+    installPhase = ''
+      mkdir -p $out/bin
+      cp $src $out/bin/ResponsivelyApp
+      chmod +x $out/bin/ResponsivelyApp
+      # Optionally, create a symlink for easier invocation
+      ln -s $out/bin/ResponsivelyApp $out/bin/responsively-app
+    '';
+
+    meta = with super.lib; {
+      description =
+        "A modified browser for fast & precise responsive web development.";
+      homepage = "https://responsively.app/";
+      license = licenses.mit;
+      platforms = platforms.linux;
+      maintainers = with maintainers; [ ];
+    };
+  };
+}
