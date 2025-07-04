3a3d19cf98713996b5dfd7fe3a0da7a9df80790f
Fractal-Tess <vgfractal@gmail.com>
2025-07-04 06:45:48 +0300

    412

--- overlays/responsively-app.nix
+++ overlays/responsively-app.nix
@@ -7,8 +7,7 @@ self: super: {
     src = super.fetchurl {
       url =
         "https://github.com/responsively-org/responsively-app-releases/releases/download/v1.16.0/ResponsivelyApp-1.16.0.AppImage";
-      sha256 =
-        "sha256-r0wznN+7zZXKNFNFUV4hm2e4gd84M6hVcun4OfNEeSw="; # TODO: Replace with real hash
+      sha256 = "sha256-r0wznN+7zZXKNFNFUV4hm2e4gd84M6hVcun4OfNEeSw=";
     };
 
     dontUnpack = true;
