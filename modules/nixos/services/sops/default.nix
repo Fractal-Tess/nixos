de4ec7b27613d07bc4fc057a6befa1278f403355
Fractal-Tess <vgfractal@gmail.com>
2025-07-06 07:23:23 +0300

    Update #11

--- modules/nixos/services/sops/default.nix
+++ modules/nixos/services/sops/default.nix
@@ -1,28 +1,16 @@
-{ config, lib, pkgs, ... }:
+{ ... }:
 
-with lib;
+{
+  # SOPS configuration for this host
+  sops.defaultSopsFile =
+    ../../../../secrets/secrets.yaml; # Path to the main secrets file
+  sops.defaultSopsFormat = "yaml"; # Format of the secrets file
+  sops.age.keyFile =
+    "/home/fractal-tess/.config/sops/age/keys.txt"; # Path to the age key file
 
-let cfg = config.modules.services.sops;
-in {
-  options.modules.services.sops = {
-    enable = mkEnableOption "Enable sops secret management";
-    age = mkOption {
-      type = types.bool;
-      default = true;
-      description = "Enable age as the backend for sops";
-    };
-  };
-
-  config = mkIf cfg.enable {
-    # Enable sops-nix module
-    environment.systemPackages = [ pkgs.sops ];
-    sops = {
-      enable = true;
-      defaultSopsFile = null; # User can override
-      age = mkIf cfg.age {
-        enable = true;
-        # The user should provide their age key(s) in /var/lib/sops/age/keys.txt or via home-manager
-      };
-    };
+  # Declare secrets to be managed by sops
+  sops.secrets = {
+    example_key = { };
+    hello = { };
   };
 }
