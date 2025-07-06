99c35591800e3dbddc06b914c1d9675a8b2095b8
Fractal-Tess <vgfractal@gmail.com>
2025-07-06 07:42:16 +0300

    Update #16

--- modules/nixos/services/sops/default.nix
+++ modules/nixos/services/sops/default.nix
@@ -6,7 +6,7 @@
     ../../../../secrets/secrets.yaml; # Path to the main secrets file
   sops.defaultSopsFormat = "yaml"; # Format of the secrets file
   sops.age.keyFile =
-    "/home/fractal-tess/.config/sops/age/keys.txt"; # Path to the age key file
+    "/home/${username}/.config/sops/age/keys.txt"; # Path to the age key file
 
   # Declare secrets to be managed by sops
   sops.secrets = {
