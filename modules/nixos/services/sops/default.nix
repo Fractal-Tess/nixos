868c26abd17881560ec3ab8cb27dbae78efde481
Fractal-Tess <vgfractal@gmail.com>
2025-07-09 23:32:14 +0300

    Update #1

--- modules/nixos/services/sops/default.nix
+++ modules/nixos/services/sops/default.nix
@@ -1,30 +1,40 @@
-{ username, ... }:
+{ config, lib, username, ... }:
 
-{
-  # SOPS configuration for this host
-  sops.defaultSopsFile =
-    ../../../../secrets/secrets.yaml; # Path to the main secrets file
-  sops.defaultSopsFormat = "yaml"; # Format of the secrets file
-  sops.age.keyFile =
-    "/home/${username}/.config/sops/age/keys.txt"; # Path to the age key file
+with lib;
 
-  # Declare secrets to be managed by sops
-  sops.secrets = {
-    example_key = {
-      owner = username;
-      path = "/var/lib/${username}/secrets";
-    };
-    hello = {
-      owner = username;
-      path = "/home/${username}/.secretv2.zsh";
-    };
+let cfg = config.modules.services.sops;
+in {
+  # Define the enable option for SOPS secrets management
+  options.modules.services.sops.enable =
+    mkEnableOption "SOPS secrets management";
 
-    # SSH authorized_keys
-    ssh_authorized_keys = {
-      owner = username;
-      sopsFile = ../../../../secrets/ssh_authorized_keys.yaml;
-      format = "yaml";
-      path = "/home/${username}/.ssh/authorized_keys";
+  # Configure SOPS only if enabled
+  config = mkIf cfg.enable {
+    # SOPS configuration for this host
+    sops.defaultSopsFile =
+      ../../../../secrets/secrets.yaml; # Path to the main secrets file
+    sops.defaultSopsFormat = "yaml"; # Format of the secrets file
+    sops.age.keyFile =
+      "/home/${username}/.config/sops/age/keys.txt"; # Path to the age key file
+
+    # Declare secrets to be managed by sops
+    sops.secrets = {
+      example_key = {
+        owner = username;
+        path = "/var/lib/${username}/secrets";
+      };
+      hello = {
+        owner = username;
+        path = "/home/${username}/.secretv2.zsh";
+      };
+
+      # SSH authorized_keys
+      ssh_authorized_keys = {
+        owner = username;
+        sopsFile = ../../../../secrets/ssh_authorized_keys.yaml;
+        format = "yaml";
+        path = "/home/${username}/.ssh/authorized_keys";
+      };
     };
   };
 }
