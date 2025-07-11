7b5fcc56a91763b6f04f0298a4f39ebb6ab5be13
Fractal-Tess <vgfractal@gmail.com>
2025-07-11 10:46:38 +0300

    Update #5

--- overlays/viber.nix
+++ overlays/viber.nix
@@ -1,8 +1,6 @@
 self: super: {
   viber_patched = super.viber.overrideAttrs (old: {
-    # Wrap the binary to include libxml2 in LD_LIBRARY_PATH
-    installPhase = ''
-      ${old.installPhase or ""}
+    preFixup = ''
       wrapProgram $out/bin/viber \
         --set LD_LIBRARY_PATH "${
           super.lib.makeLibraryPath [ super.libxml2 ]
