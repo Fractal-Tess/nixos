b849787fdb9cd3fda1901a6d9d7da4a69be988ae
Fractal-Tess <vgfractal@gmail.com>
2025-07-11 10:43:26 +0300

    Update #4

--- hosts/vd/home.nix
+++ hosts/vd/home.nix
@@ -152,7 +152,7 @@ in {
     discord # Voice, video and text chat
     telegram-desktop # Messaging app
     bitwarden # Password manager
-    viber # Voice and messaging app
+    viber_patched # Voice and messaging app
     slack # Team collaboration platform
     thunderbird # Email client
 
