481b21b6cf3b0bfa0a5120b1e2f0b9a532d6b657
Fractal-Tess <vgfractal@gmail.com>
2025-07-11 11:02:29 +0300

    Update #8

--- overlays/viber.nix
+++ overlays/viber.nix
@@ -8,12 +8,15 @@ self: super: {
 
     installPhase = ''
       mkdir -p $out/bin
+      # Remove the symlink if it exists
+      rm -f $out/bin/viber
+      # Create a wrapper script instead
       cat > $out/bin/viber <<EOF
       #!${super.stdenv.shell}
       export LD_LIBRARY_PATH="${
         super.lib.makeLibraryPath [ super.libxml2 ]
       }:$''${LD_LIBRARY_PATH:-}"
-      exec -a "$0" "${super.viber}/bin/viber" "$@"
+      exec -a "$0" "${super.viber}/opt/viber/Viber" "$@"
       EOF
       chmod +x $out/bin/viber
     '';
