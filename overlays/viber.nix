16074ad2663cc811fbfdd41b29e906709144a49a
Fractal-Tess <vgfractal@gmail.com>
2025-07-11 11:12:55 +0300

    Update #10

--- overlays/viber.nix
+++ overlays/viber.nix
@@ -1,5 +1,5 @@
 self: super: {
-  # Overlay for Viber official AppImage
+  # Overlay for Viber official AppImage, wrapped with appimage-run for NixOS compatibility
   viber-appimage = super.stdenv.mkDerivation {
     pname = "viber-appimage";
     version = "latest"; # Update if you want to pin a version
@@ -8,22 +8,25 @@ self: super: {
     src = super.fetchurl {
       url = "https://download.cdn.viber.com/desktop/Linux/viber.AppImage";
       sha256 =
-        "sha256-S+PpVbMq30p6PECUfdp2FESbqFk9lTbaadNFUDs7TkE="; # TODO: Replace with real hash after first build
+        "sha256-S+PpVbMq30p6PECUfdp2FESbqFk9lTbaadNFUDs7TkE="; # Verified hash
     };
 
+    # Add appimage-run and makeWrapper to buildInputs
+    buildInputs = [ super.appimage-run super.makeWrapper ];
     dontUnpack = true;
 
     installPhase = ''
       # Create the output bin directory
       mkdir -p $out/bin
-      # Copy the AppImage to the output bin directory
-      cp $src $out/bin/viber
-      # Make the AppImage executable
-      chmod +x $out/bin/viber
+      # Copy the AppImage to the output directory
+      cp $src $out/viber.AppImage
+      # Create a wrapper that runs the AppImage with appimage-run
+      makeWrapper ${super.appimage-run}/bin/appimage-run $out/bin/viber \
+        --add-flags "$out/viber.AppImage"
     '';
 
     meta = {
-      description = "Viber official AppImage";
+      description = "Viber official AppImage (wrapped for NixOS)";
       homepage = "https://www.viber.com/";
       license = super.lib.licenses.unfree;
       maintainers = with super.lib.maintainers; [ ];
