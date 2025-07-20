0756654807710a8a9d731b6c3cb3b7e0efab2d88
Fractal-Tess <vgfractal@gmail.com>
2025-07-21 00:44:34 +0300

    Update #28

--- modules/nixos/services/virtualization/docker.nix
+++ modules/nixos/services/virtualization/docker.nix
@@ -30,9 +30,6 @@ in {
       # --restart=always flag to work. 
       enableOnBoot = true;
 
-      # Enable Nvidia support if enabled
-      enableNvidia = cfg.nvidia;
-
       # Configure the Docker to run in rootless mode 
       rootless = mkIf cfg.rootless {
         enable = true;
