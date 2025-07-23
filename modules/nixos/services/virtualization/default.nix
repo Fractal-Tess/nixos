285ea68c3b2291904de59483161390b7c1e63962
Fractal-Tess <vgfractal@gmail.com>
2025-07-23 17:18:32 +0300

    Update #34

--- modules/nixos/services/virtualization/default.nix
+++ modules/nixos/services/virtualization/default.nix
@@ -5,6 +5,8 @@
     ./kubernetes.nix
     ./podman.nix
 
+    # Import backup utilities first so they're available to container modules
+    ./containers/backup-utils.nix
     ./containers/portainer.nix
     ./containers/jellyfin.nix
     ./containers/netdata.nix
