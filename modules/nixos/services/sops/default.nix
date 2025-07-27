e6be1e6a4824a19a98e461b07caefde26e50fe6f
Fractal-Tess <vgfractal@gmail.com>
2025-07-27 14:34:27 +0300

    Update #4

--- modules/nixos/services/sops/default.nix
+++ modules/nixos/services/sops/default.nix
@@ -4,44 +4,18 @@ with lib;
 
 let cfg = config.modules.services.sops;
 in {
-  # Define the enable option for SOPS secrets management
-  options.modules.services.sops.enable =
-    mkEnableOption "SOPS secrets management";
 
-  # Configure SOPS only if enabled
+  imports = [ ./ssh.nix ];
+
+  options.modules.services.sops = {
+    enable = mkEnableOption "SOPS secrets management";
+  };
+
   config = mkIf cfg.enable {
-    # SOPS configuration for this host
-    sops.defaultSopsFile =
-      ../../../../secrets/secrets.yaml; # Path to the main secrets file
-    sops.defaultSopsFormat = "yaml"; # Format of the secrets file
-    sops.age.keyFile =
-      "/home/${username}/.config/sops/age/keys.txt"; # Path to the age key file
-
-    # Declare secrets to be managed by sops
-    sops.secrets = {
-      example_key = {
-        owner = username;
-        path = "/var/lib/${username}/secrets";
-      };
-      hello = {
-        owner = username;
-        path = "/home/${username}/.secretv2.zsh";
-      };
-
-      # SSH authorized_keys
-      ssh_authorized_keys = {
-        owner = username;
-        sopsFile = ../../../../secrets/ssh_authorized_keys.yaml;
-        format = "yaml";
-        path = "/home/${username}/.ssh/authorized_keys";
-      };
-
-      ssh_config = {
-        owner = username;
-        sopsFile = ../../../../secrets/ssh_config.yaml;
-        format = "yaml";
-        path = "/home/${username}/.ssh/config";
-      };
+    sops = {
+      defaultSopsFile = ../../../../secrets/secrets.yaml;
+      age.keyFile = "/home/${username}/.config/sops/age/keys.txt";
     };
   };
+
 }
