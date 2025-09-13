38f2da2f76a85e3d282355bf84c61e3a4d9ee1ad
Fractal-Tess <vgfractal@gmail.com>
2025-09-14 02:18:57 +0300

    Update #12

--- modules/nixos/display/ly.nix
+++ modules/nixos/display/ly.nix
@@ -6,13 +6,11 @@ let
   cfg = config.modules.display.ly;
   hyprland = config.modules.display.hyprland;
 
-in {
+in
+{
   options.modules.display.ly = {
     # Option to enable/disable the ly display manager
     enable = mkEnableOption "ly display manager";
-
-    # Option to enable automatic login without password prompt
-    autoLogin = mkEnableOption "ly auto login";
   };
 
   # Configuration that applies when this module is enabled
@@ -27,31 +25,7 @@ in {
     # Enable ly display manager
     services.displayManager.ly = {
       enable = true;
-      settings = {
-        # Basic ly configuration
-        animation = "none";
-        bigclock = false;
-        clear_password = true;
-        hide_borders = false;
-        hide_f1_commands = false;
-        load_config = true;
-        save_file = "/tmp/ly-save";
-        term_reset_cursor = true;
-      };
     };
-
-    # Configure auto login if enabled
-    services.displayManager.autoLogin = mkIf cfg.autoLogin {
-      enable = true;
-      user = username;
-    };
-
-    # Set default session to Hyprland with UWSM
-    services.displayManager.defaultSession = "hyprland-uwsm";
-    
-    # Add UWSM session configuration
-    services.displayManager.sessionPackages = [ 
-      config.programs.hyprland.package
-    ];
   };
 }
+
