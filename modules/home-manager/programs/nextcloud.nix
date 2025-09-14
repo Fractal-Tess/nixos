fbd03f06f8689ec1ddb82a5cdf0f4deb82b0e4ce
Fractal-Tess <vgfractal@gmail.com>
2025-09-14 06:29:39 +0300

    Update #31

--- modules/home-manager/programs/nextcloud.nix
+++ modules/home-manager/programs/nextcloud.nix
@@ -1,6 +1,19 @@
-{ pkgs, ... }: {
-  services.nextcloud-client = {
-    enable = true;
-    startInBackground = true;
+{ config, lib, pkgs, ... }:
+
+with lib;
+
+let
+  cfg = config.modules.programs.nextcloud;
+in
+{
+  options.modules.programs.nextcloud = {
+    enable = mkEnableOption "Nextcloud client";
+  };
+
+  config = mkIf cfg.enable {
+    services.nextcloud-client = {
+      enable = true;
+      startInBackground = true;
+    };
   };
 }
