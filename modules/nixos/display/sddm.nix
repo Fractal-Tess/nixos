dc7e3495eba40ac685eb94f1b7fc252e1f66f2c5
Fractal-Tess <vgfractal@gmail.com>
2025-09-14 05:42:18 +0300

    Update #24

--- modules/nixos/display/sddm.nix
+++ modules/nixos/display/sddm.nix
@@ -11,7 +11,7 @@ in
   options.modules.display.sddm = {
     # Option to enable/disable SDDM display manager
     enable = mkEnableOption "SDDM display manager";
-    
+
     # Option to enable the astronaut theme
     enableAstronautTheme = mkEnableOption "SDDM Astronaut theme" // { default = true; };
   };
@@ -25,25 +25,17 @@ in
         "SDDM is configured to use Hyprland, but Hyprland is not enabled. Please enable modules.display.hyprland.";
     }];
 
-    # Install SDDM astronaut theme package
-    environment.systemPackages = mkIf cfg.enableAstronautTheme [
-      pkgs.sddm-astronaut
-    ];
-
     # Enable SDDM display manager
     services.displayManager.sddm = {
       enable = true;
       wayland.enable = true;
+      package = pkgs.kdePackages.sddm;
       theme = mkIf cfg.enableAstronautTheme "sddm-astronaut-theme";
-      # Configuration for the black hole variant
-      settings = mkIf cfg.enableAstronautTheme {
-        Theme = {
-          ConfigFile = "Themes/blackhole.conf";
-        };
-      };
+      extraPackages = mkIf cfg.enableAstronautTheme [ pkgs.sddm-astronaut ];
     };
 
     # Use Hyprland UWSM session (as shown in error message)
     services.displayManager.defaultSession = "hyprland-uwsm";
   };
 }
+
