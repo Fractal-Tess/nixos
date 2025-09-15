8e1731b52b02e79e96ba2d1ca4461e6e02f4ec16
Fractal-Tess <vgfractal@gmail.com>
2025-09-15 09:01:21 +0300

    Update #5

--- hosts/neo/configuration.nix
+++ hosts/neo/configuration.nix
@@ -140,8 +140,8 @@
     tlp = {
       enable = true;
       settings = {
-        CPU_SCALING_GOVERNOR_ON_AC = "powersave";
-        CPU_ENERGY_PERF_POLICY_ON_AC = "powersave";
+        CPU_SCALING_GOVERNOR_ON_AC = "performance";
+        CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
 
         CPU_MIN_PERF_ON_AC = 0;
         CPU_MAX_PERF_ON_AC = 100;
