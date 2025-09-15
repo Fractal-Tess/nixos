7c896862d160adad72468a0242ee285e3eb5bb73
Fractal-Tess <vgfractal@gmail.com>
2025-09-15 07:02:18 +0300

    Update #39

--- modules/nixos/display/hyprland.nix
+++ modules/nixos/display/hyprland.nix
@@ -6,20 +6,12 @@ in {
   # Options for Hyprland configuration
   options.modules.display.hyprland = {
     enable = mkEnableOption "Hyprland";
-    
-    autoLogin = mkOption {
-      type = types.bool;
-      default = false;
-      description = "Enable automatic login for the user";
-    };
   };
 
   config = mkIf cfg.enable {
     # Enable Hyprland compositor
     programs.hyprland = {
       enable = true;
-
-      package = pkgs.hyprland;
       portalPackage = pkgs.xdg-desktop-portal-hyprland;
 
       # Enable Xwayland
@@ -28,16 +20,12 @@ in {
       withUWSM = true;
     };
 
-    # Enable auto login (conditional)
-    services.getty.autologinUser = mkIf cfg.autoLogin username;
-
     # Hardware acceleration
-    hardware.graphics = {
-      enable = true;
-      # 32-bit support
-      enable32Bit = true;
-
-    };
+    # hardware.graphics = {
+    #   enable = true;
+    #   # 32-bit support
+    #   enable32Bit = true;
+    # };
 
     # Enable Hyprlock
     programs.hyprlock.enable = true;
@@ -47,16 +35,16 @@ in {
     # Enable XDG Desktop Portal for sandboxed/Wayland apps
     xdg.portal = {
       enable = mkDefault true;
-      # Use portal for xdg-open
+      #   # Use portal for xdg-open
       xdgOpenUsePortal = mkDefault true;
-      # Add GTK portal backend
+      #   # Add GTK portal backend
       extraPortals = mkDefault [ pkgs.xdg-desktop-portal-gtk ];
     };
 
     # Add kitty terminal to system packages
     environment.systemPackages = [ pkgs.kitty ];
     # Enable dconf for GTK/Flatpak app settings
-    programs.dconf.enable = true;
+    # programs.dconf.enable = true;
 
     # Add nvidia driver for
     services.xserver.videoDrivers = mkMerge [
