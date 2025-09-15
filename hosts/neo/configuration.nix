066d5943be316cf72583877dcb7f85253e1237c9
Fractal-Tess <vgfractal@gmail.com>
2025-09-15 08:49:55 +0300

    Update #2

--- hosts/neo/configuration.nix
+++ hosts/neo/configuration.nix
@@ -142,21 +142,12 @@
       enable = true;
       settings = {
         CPU_SCALING_GOVERNOR_ON_AC = "powersave";
-        CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
-
         CPU_ENERGY_PERF_POLICY_ON_AC = "powersave";
-        CPU_ENERGY_PERF_POLICY_ON_BAT = "powersave";
-
-        # CPU_MAX_FREQ_ON_AC = 3100000;
-        # CPU_MAX_FREQ_ON_BAT = 3100000;
 
         CPU_MIN_PERF_ON_AC = 0;
-        CPU_MAX_PERF_ON_AC = 50;
-        CPU_MIN_PERF_ON_BAT = 0;
-        CPU_MAX_PERF_ON_BAT = 30;
+        CPU_MAX_PERF_ON_AC = 100;
 
         CPU_BOOST_ON_AC = 0; # disable boost on AC
-        CPU_BOOST_ON_BAT = 0; # disable boost on battery
       };
     };
   };
