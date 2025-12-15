62e425d375590b6b8501d6f04eb8a5a45cbed9ea
Fractal-Tess <vgfractal@gmail.com>
2025-12-15 22:01:19 +0200

    Update #35

--- modules/home-manager/configs/hyprland/startup.nix
+++ modules/home-manager/configs/hyprland/startup.nix
@@ -7,7 +7,7 @@
     exec-once = wl-paste --watch cliphist store
     exec-once = blueman-applet
     exec-once = nm-applet
-    exec-once = netbird-ui
+    # exec-once = netbird-ui
     exec-once = hypridle
   '';
 }
