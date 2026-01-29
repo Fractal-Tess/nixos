a326ee12afd1be6b02cf374d9f059d304ef72dc2
Fractal-Tess <vgfractal@gmail.com>
2026-01-29 17:27:49 +0200

    Update #88

--- modules/home-manager/configs/hyprland/windows.nix
+++ modules/home-manager/configs/hyprland/windows.nix
@@ -9,15 +9,15 @@
     # See https://wiki.hyprland.org/Configuring/Window-Rules/ for more
     # See https://wiki.hyprland.org/Configuring/Workspace-Rules/ for workspace rules
 
-    windowrule = min_size 200 400, float, match:title ^(UnityEngine.*)
-    windowrule = min_size 200 400, float, match:title ^(UnityEditor.*)
-    windowrule = float, match:class ^(ghostty)$
-    windowrule = float, match:class ^(dev.warp.Warp)$
-    windowrule = float, match:title ^(File Operation Progress)$
-    windowrule = workspace 1, match:class ^(discord)$
-    windowrule = workspace 2, match:class ^(dev.zed.Zed)$
-    windowrule = workspace 2, match:class ^(cursor)$
-    windowrule = workspace 3, match:class ^(Vivaldi-stable)$
-    windowrule = suppress_event maximize, match:class .*
+    windowrule = match:title ^(UnityEngine.*), min_size 200 400, float on
+    windowrule = match:title ^(UnityEditor.*), min_size 200 400, float on
+    windowrule = match:class ^(ghostty)$, float on
+    windowrule = match:class ^(dev.warp.Warp)$, float on
+    windowrule = match:title ^(File Operation Progress)$, float on
+    windowrule = match:class ^(discord)$, workspace 1
+    windowrule = match:class ^(dev.zed.Zed)$, workspace 2
+    windowrule = match:class ^(cursor)$, workspace 2
+    windowrule = match:class ^(Vivaldi-stable)$, workspace 3
+    windowrule = match:class .*, suppress_event maximize
   '';
 }
