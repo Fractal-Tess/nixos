89122d438b3700bd2d6f6df97ba4acf4187884f2
Fractal-Tess <vgfractal@gmail.com>
2026-02-01 19:35:53 +0200

    Update #95

--- modules/home-manager/configs/fish/functions/_ncs_setup.fish
+++ modules/home-manager/configs/fish/functions/_ncs_setup.fish
@@ -4,18 +4,32 @@
 function _ncs_setup
   set -l lang $argv[1]
   set -l target_dir "$HOME/nixos/shells/$lang"
-  
-  if test -d "$target_dir"
-    cp -r "$target_dir"/* "$PWD/"
-    echo "use flake" > .envrc
-    
-    if test -d .git
-      git add flake.lock flake.nix .envrc
-      direnv allow
-    end
-    
-    echo "Direnv for $lang has been set up. Happy coding!"
-  else
+
+  if test ! -d "$target_dir"
     echo "No development shell found for $lang"
+    return 1
   end
+
+  # Copy flake.nix and flake.lock
+  cp "$target_dir/flake.nix" "$PWD/"
+  cp "$target_dir/flake.lock" "$PWD/"
+
+  # Copy .envrc if it exists in the shell template
+  if test -f "$target_dir/.envrc"
+    cp "$target_dir/.envrc" "$PWD/"
+  else
+    # Create default .envrc
+    echo "use flake" > .envrc
+  end
+
+  # Initialize git if not present
+  if test ! -d .git
+    git init
+  end
+
+  # Add files to git and allow direnv
+  git add flake.lock flake.nix .envrc
+  direnv allow
+
+  echo "Direnv for $lang has been set up. Happy coding!"
 end
