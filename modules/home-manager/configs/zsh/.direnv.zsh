6c56342654dde5b1ee57a8ace8d792ee1d1f6076
Fractal-Tess <vgfractal@gmail.com>
2026-02-01 19:35:53 +0200

    Update #95

--- modules/home-manager/configs/zsh/.direnv.zsh
+++ modules/home-manager/configs/zsh/.direnv.zsh
@@ -1,19 +1,2 @@
 
-# Direnv & Shell helpers 
-function _ncs_setup() {
-cp ~/nixos/shells/$1/{flake.nix,flake.lock} ./
-_git_init_flake
-_direnv_init
-}
-
-function _git_init_flake() {
-if [ ! -d .git ]; then
-    git init
-fi
-git add flake.nix flake.lock
-}
-
-function _direnv_init() {
-echo 'use flake' > .envrc
-direnv allow
-}
+# Direnv & Shell helpers
