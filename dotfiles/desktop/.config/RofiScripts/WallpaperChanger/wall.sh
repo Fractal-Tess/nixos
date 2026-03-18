76550e002a9fdea5abad25d65f7d397fd8f8ebc8
Fractal-Tess <vgfractal@gmail.com>
2026-03-18 20:18:28 +0200

    Update wallpaper scripts and color schemes in NixOS config

--- dotfiles/desktop/.config/RofiScripts/WallpaperChanger/wall.sh
+++ dotfiles/desktop/.config/RofiScripts/WallpaperChanger/wall.sh
@@ -5,4 +5,4 @@ selected=$(ls "$DIR" | while read -r A; do
   echo -en "$A\x00icon\x1f$DIR/$A\n"
 done | rofi -dmenu -i -config '~/.config/RofiScripts/WallpaperChanger/WC.rasi')
 [ -z "$selected" ] && exit 0
-matugen image "$DIR/$selected" -m dark -t scheme-fidelity --fallback-color grey
+matugen image "$DIR/$selected" -m dark -t scheme-fidelity --fallback-color '#888888' --source-color-index 0
