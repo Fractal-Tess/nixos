3513e8b8999eac5165012eaad36fa93f248db86b
Fractal-Tess <vgfractal@gmail.com>
2026-03-20 18:46:27 +0200

    clean up hosts/vd/home.nix and update fish.nix

--- hosts/vd/home.nix
+++ hosts/vd/home.nix
@@ -20,15 +20,6 @@
     username = username;
     homeDirectory = "/home/${username}";
     stateVersion = "25.05"; # Don't change this
-    sessionPath = [
-      "$HOME/nixos/scripts"
-      "$HOME/nixos/scripts/bin"
-      "$HOME/nixos/scripts/hyprland"
-      "$HOME/nixos/scripts/nixos"
-      "$HOME/nixos/scripts/wallpaper"
-      "$HOME/nixos/scripts/waybar"
-      "$HOME/nixos/scripts/claude-code"
-    ];
     sessionVariables = {
       PNPM_HOME = "$HOME/.local/share/pnpm";
       PLAYWRIGHT_BROWSERS_PATH = "${pkgs.playwright-driver.browsers}";
