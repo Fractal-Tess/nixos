5a087f49f0e67532d477982b00a82b10089fd836
Fractal-Tess <vgfractal@gmail.com>
2026-03-23 18:36:22 +0200

    update desktop color schemes and hypr config files

--- flake.nix
+++ flake.nix
@@ -84,6 +84,8 @@
         };
     in
     {
+      formatter.x86_64-linux = nixpkgs.legacyPackages.x86_64-linux.nixfmt;
+
       nixosConfigurations = {
         vd = mkHost {
           hostname = "vd";
