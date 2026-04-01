7cf666a7d568e7e4a0de85649295da8ddfcb086e
Fractal-Tess <vgfractal@gmail.com>
2026-04-01 19:15:13 +0300

    update kiwi and vd host configs and home.nix files

--- hosts/vd/home.nix
+++ hosts/vd/home.nix
@@ -1,4 +1,9 @@
-{ pkgs, username, ... }:
+{
+  pkgs,
+  username,
+  lib,
+  ...
+}:
 
 {
   #============================================================================
@@ -21,10 +26,25 @@
     stateVersion = "25.05"; # Don't change this
     sessionVariables = {
       PNPM_HOME = "$HOME/.local/share/pnpm";
-      PLAYWRIGHT_BROWSERS_PATH = "${pkgs.playwright-driver.browsers}";
+      PLAYWRIGHT_BROWSERS_PATH = "$HOME/.local/share/playwright-browsers";
       PLAYWRIGHT_SKIP_VALIDATE_HOST_REQUIREMENTS = "true";
       AGENT_BROWSER_EXECUTABLE_PATH = "${pkgs.google-chrome}/bin/google-chrome";
     };
+
+    activation.setupPlaywrightBrowsers = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
+      BROWSERS_DIR="$HOME/.local/share/playwright-browsers"
+      $DRY_RUN_CMD mkdir -p "$BROWSERS_DIR"
+      for dir in ${pkgs.playwright-driver.browsers}/*/; do
+        name=$(basename "$dir")
+        target="$BROWSERS_DIR/$name"
+        if [ -L "$target" ] && [ "$(readlink "$target")" != "$dir" ]; then
+          $DRY_RUN_CMD rm "$target"
+        fi
+        if [ ! -e "$target" ]; then
+          $DRY_RUN_CMD ln -s "$dir" "$target"
+        fi
+      done
+    '';
   };
 
   # Enable Home Manager self-management
