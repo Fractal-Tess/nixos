13e84d738cef0c66cedd4d7a0735c67b595ba3da
Fractal-Tess <vgfractal@gmail.com>
2026-04-04 22:47:00 +0300

    Update wallpaper scripts and host vd configs (btop,waybar,waypaper)

--- dotfiles/desktop/.config/RofiScripts/WallpaperChanger/wallrandom.sh
+++ dotfiles/desktop/.config/RofiScripts/WallpaperChanger/wallrandom.sh
@@ -1,18 +1,25 @@
 #!/bin/sh
 
+set -eu
+
 DIR="$HOME/wallpapers/new"
 LAST_WALLPAPER="$HOME/.config/RofiScripts/WallpaperChanger/last_wallpaper.txt"
 
-files=("$DIR"/*)
-[ ${#files[@]} -eq 0 ] && exit 0
+[ -d "$DIR" ] || exit 0
+
+count=$(find "$DIR" -maxdepth 1 -type f | wc -l)
+[ "$count" -eq 0 ] && exit 0
 
 last=""
 [ -f "$LAST_WALLPAPER" ] && last=$(cat "$LAST_WALLPAPER")
 
-while true; do
-    selected=$(find "$DIR" -type f | shuf -n 1)
-    [ "$selected" != "$last" ] && break
-done
+selected=$(find "$DIR" -maxdepth 1 -type f | shuf -n 1)
+if [ "$count" -gt 1 ]; then
+  while [ "$selected" = "$last" ]; do
+    selected=$(find "$DIR" -maxdepth 1 -type f | shuf -n 1)
+  done
+fi
 
+waypaper --backend awww --wallpaper "$selected"
 matugen image "$selected" -m dark -t scheme-fidelity --fallback-color '#888888' --source-color-index 0
 echo "$selected" > "$LAST_WALLPAPER"
